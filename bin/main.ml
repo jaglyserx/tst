aa4b4f8fcbc72069f0f72ffda9cb0bba4a21da21
jaglyserx <joelwblomster@gmail.com>
2026-09-13 11:44:44 +0200

    setup entry point

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,5 @@
-let () = print_endline "Hello, World!"
+let () = 
+    match Array.length Sys.argv with
+    | 1 -> print_endline "here's for one"
+    | 2 -> print_endline "here's for two"
+    | _ -> prerr_endline "here's err"
